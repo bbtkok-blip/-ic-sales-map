5c18a854c4815de1bdb76703978927cedf008eaa
bbtkok-blip <bb.tkok@gmail.com>
2026-08-25 15:37:49 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE='ic-sales-map-v2';
+const CACHE='ic-sales-map-v4-multisheet';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',e=>{
-  e.respondWith(fetch(e.request).then(r=>{
+  e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{
     const copy=r.clone();
     caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
     return r;
